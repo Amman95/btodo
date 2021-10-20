eab4092673652b4e30ae90e316e255b4d7b845c7
HafiniAina <hafiniaina97@gmail.com>
2021-10-20 17:13:51 +0800

    finiset

--- src/App.js
+++ src/App.js
@@ -1,25 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import data_array from "./obj.js";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Education from "./Education";
+import Recreational from "./Recreational";
+import Social from "./Social";
+import Diy from "./Diy";
+import Charity from "./Charity";
+import Cooking from "./Cooking";
+import Relaxation from "./Relaxation";
+import Music from "./Music";
+import Busywork from "./Busywork";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          keep trying ye babeee!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  var filtered = data_array.filter((data) => data.type === "education");
+  var recommends = data_array.filter(
+    (data) =>
+      data.price === 0.0 && data.accessibility === "Few to no challenges"
+  );
+  const mappingFil = (
+    <div className="category">
+      {filtered.map((list) => {
+        return (
+          <div id="card" key={list.id}>
+            <p>Activity: {list.activity}</p>
+            <p>No. of participants: {list.participants}</p>
+            <p>Type: {list.type}</p>
+          </div>
+        );
+      })}
     </div>
   );
+
+  const mappingRecs = (
+    <div className="recommended">
+      {recommends.slice(0, 5).map((list) => {
+        return (
+          <div id="card" key={list.id}>
+            <p>Activity: {list.activity}</p>
+            <p>No. of participants: {list.participants}</p>
+            <p>Type: {list.type}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+
+  return (
+    <>
+      <Router>
+        <div className="App">
+          <h1>Bored?</h1>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/home" exact component={Home} />
+            <Route path="/education" component={Education} />
+            <Route path="/recreational" component={Recreational} />
+            <Route path="/social" component={Social} />
+            <Route path="/diy" component={Diy} />
+            <Route path="/charity" component={Charity} />
+            <Route path="/cooking" component={Cooking} />
+            <Route path="/relaxation" component={Relaxation} />
+            <Route path="/music" component={Music} />
+            <Route path="/busywork" component={Busywork} />
+          </Switch>
+        </div>
+      </Router>
+      {/* <div id="bored-container">
+        <h2>Recommended</h2>
+        {mappingRecs}
+      </div> */}
+    </>
+  );
 }
+
+const Home = () => (
+  <div>
+    <p>Categories:</p>
+    <Link to="/education">
+      <button>Education</button>
+    </Link>
+    <Link to="/recreational">
+      <button>Recreational</button>
+    </Link>
+    <Link to="/social">
+      <button>Social</button>
+    </Link>
+    <Link to="/diy">
+      <button>DIY</button>
+    </Link>
+    <Link to="/charity">
+      <button>Charity</button>
+    </Link>
+    <Link to="/cooking">
+      <button>Cooking</button>
+    </Link>
+    <Link to="/relaxation">
+      <button>Relaxation</button>
+    </Link>
+    <Link to="/music">
+      <button>Music</button>
+    </Link>
+    <Link to="/busywork">
+      <button>Busywork</button>
+    </Link>
+  </div>
+);
 
 export default App;
---
+++ src/home.js
@@ -0,0 +1,78 @@
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import { BrowserRoute as Router, Route, Switch, Link } from "react-router-dom";
+import Education from "./Education";
+import { useHistory } from "react-router-dom";
+
+function Home() {
+
+  
+
+  const [ActivityDet, setADetails] = useState("");
+  const [Activity, setActivity] = useState("");
+  const [Type, setType] = useState("");
+  const [Participant, setParticipant] = useState("");
+
+  useEffect(() => {
+    fetch("https://www.boredapi.com/api/activity/")
+      .then((response) => response.json())
+      .then((data) => setADetails(data));
+  }, []);
+
+  return (
+    <>
+      <div id="card-container">
+        <div id="card">
+           <div class="act"></div>
+          <p>Activity: {ActivityDet.activity}</p>
+          <p>No. of participants: {ActivityDet.participants}</p>
+          <p>Type: {ActivityDet.type}</p>
+        </div>
+      </div>
+
+      <div id="categories">
+        <Router>
+          <ul>
+            <li>
+              <Link to="/Education">Education</Link>
+            </li>
+            <li>
+              <Link to="/recreational">Recreational</Link>
+            </li>
+            <li>
+              <Link to="/social">Social</Link>
+            </li>
+            <li>
+              <Link to="/diy">DIY</Link>
+            </li>
+            <li>
+              <Link to="/charity">Charity</Link>
+            </li>
+            <li>
+              <Link to="/cooking">Cooking</Link>
+            </li>
+            <li>
+              <Link to="/relaxation">Relaxation</Link>
+            </li>
+            <li>
+              <Link to="/music">Music</Link>
+            </li>
+            <li>
+              <Link to="/busywork">Busywork</Link>
+            </li>
+          </ul>
+          <Switch>
+            <Route exact path="/Home">
+              <Home />
+            </Route>
+            <Route exact path="/Education">
+              <Education />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    </>
+  );
+}
+
+export default Home;
